d533ee8fc67e7f998e41c2556727453659bb95c5
Przemysław Pietrzak <przemyslaw.pietrzak@comarch.com>
2021-01-20 21:34:39 +0100

    added storing language to local storage

--- src/contexts/localization/reducer.ts
+++ src/contexts/localization/reducer.ts
@@ -2,8 +2,19 @@ import { LANGUAGE_CHANGED_ACTION } from "./actions";
 import { availableLanguages, getLanguageById, Languages } from "./languages";
 import { _ILanguageContextState, _LanguageContextAction } from "./types";
 
+const LOCAL_STORAGE_LANGUAGE = 'lang';
+
+const loadDefaultLanguage = () => {
+    const storedLangAsString = localStorage.getItem(LOCAL_STORAGE_LANGUAGE);
+    const storedLangCode = storedLangAsString ? storedLangAsString as keyof typeof Languages : null;
+    const storedLang = storedLangCode ? Languages[storedLangCode] : null;
+    return storedLang || Languages.POLISH;
+}
+
+const DEFAULT_LANGUAGE = getLanguageById(loadDefaultLanguage());
+
 export const _initialLanguageContextState: _ILanguageContextState = {
-    selectedLanguage: getLanguageById(Languages.POLISH),
+    selectedLanguage: DEFAULT_LANGUAGE,
     languages: availableLanguages
 };
 
@@ -11,8 +22,10 @@ const reducer = (state: _ILanguageContextState, action: _LanguageContextAction<a
     switch (action.type) {
         case LANGUAGE_CHANGED_ACTION:
             const correctAction = (action as _LanguageContextAction<Languages>);
-            const targetLanguageId = correctAction.payload || Languages.POLISH;
-            const targetLanguage = getLanguageById(targetLanguageId);
+            const targetLanguage = correctAction.payload !== undefined ? getLanguageById(correctAction.payload) : DEFAULT_LANGUAGE;
+            console.log('action', correctAction);
+            console.log('targetId', targetLanguage);
+            localStorage.setItem(LOCAL_STORAGE_LANGUAGE, Languages[targetLanguage.id]);
             return {
                 ...state,
                 selectedLanguage: targetLanguage
